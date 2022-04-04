039c4db811932325c61b53751676712f81ad1e15
fanj1216yxing <36510359+fanj1216yxing@users.noreply.github.com>
2022-04-04 11:44:36 +0800

    Create imageapi.php

---
+++ imageapi.php
@@ -0,0 +1,37 @@
+<?php
+
+$arr = array(
+
+'https://api.ixiaowai.cn/api/api.php',
+
+'http://www.dmoe.cc/random.php',
+
+'https://api.ixiaowai.cn/api/api.php',
+
+'https://www.yingciyuan.cn/api.php',
+
+'https://api.ghser.com/random/api.php',
+
+'https://tuapi.eees.cc/api.php?category={dongman,fengjing}',
+
+'https://tenapi.cn/acg',
+
+'https://api.qicaiyun.top/ercy/api.php',
+
+'https://tu.ecy.ink/%E4%BA%8C%E6%AC%A1%E5%85%83/random.php',
+
+'https://api.likepoems.com/pc',
+
+'http://api.x-xh.cn/xjg/z.php'
+
+);
+
+$key = array_rand($arr, 1);
+
+//得到跳转网址
+
+// echo $arr[$key];
+
+header('Location: ' . $arr[$key]);
+
+exit;
